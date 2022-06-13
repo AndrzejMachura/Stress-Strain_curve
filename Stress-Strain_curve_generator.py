56daed01b0b79af80f0b1912382d79dc100ba46f
unknown <andrzej.jakub.machura@gmail.com>
2022-06-13 15:04:08 +0200

    initial script version

---
+++ Stress-Strain_curve_generator.py
@@ -0,0 +1,59 @@
+"""This script is responsible for generating non-linear Stress-Strain curve usig Ramberg-Osgood relation  
+   and linear curve acording to Hooke's law.
+   The output of this script are:
+   1. Text file containig elasto-plastic and linear defintinion of given material
+   2. Chart with drawn Stress-Strain curve of given material.
+"""
+import pylab
+import isotropic_material
+
+def generate_chart(stress,x_l,x_ep):
+
+    ultimate_elongation = [0., 1.1*max(x_ep)]
+    ultimate_stress = [Ftu, Ftu]
+
+    pylab.title("Stress-Strain")
+    pylab.plot(x_l,stress,'y', x_ep,stress,'b',ultimate_elongation,ultimate_stress,'r')
+    pylab.legend(['Linear Stress-Strain','Elasto-plastic Stress-Strain','Ultimate Stregnth'], loc='lower right', shadow=True, fontsize='medium', title='Legend')
+    pylab.ylim(0., 1.1*max(stress))
+    pylab.ylabel("Stress [MPa]")
+    pylab.xlim(0., 1.1*max(x_ep))
+    pylab.xlabel("Strain [-]") 
+    pylab.grid(True)  
+    pylab.savefig(str(mat_name)+".jpg", dpi = 720) 
+    pylab.show()
+
+def generate_text_file(X):
+    file = open(X.mat_name+".txt","w")
+    file.write(str(X)+"\n")
+    file.write("Row Number\tLinear Strain\tTrue Strain\tStress \n")
+    i=0
+    while i < len(material.generate_stress_list()):
+        x_l = "{:.3e}".format(material.generate_hooke_strain()[i])
+        x_ep = "{:.3e}".format(material.generate_r_o_strain()[i])
+        y = "{:.3e}".format(material.generate_stress_list()[i])
+        file.write(str(i+1)+"\t"+str(x_l)+"\t"+str(x_ep)+"\t"+str(y)+"\n")
+        i+=1
+    
+    file.close()
+
+print("This script is responsible for generating non-linear Stress-Strain curve usig Ramberg-Osgood relation and linear curve acording to Hooke's law.")
+print("Required units are given in brackets \n")
+# material properties loading
+"""
+mat_name= input("Give material name: ")
+E = float (input("Give Younga modulus (MPa): "))
+Ftu = float (input("Give Ultimate Tensile Strength (MPa): "))
+Fty = float (input("Give Yield Tensile Strength (MPa): ")) 
+u = float (input("Give maximum elongation (%): "))
+"""
+mat_name= "AISI_301"
+E = 200000
+Ftu = 500
+Fty = 179
+u = 20
+
+material = isotropic_material.mat(mat_name, Ftu, Fty, u, E)
+
+generate_chart(material.generate_stress_list(), material.generate_hooke_strain(), material.generate_r_o_strain())
+generate_text_file(material)
---
+++ isotropic_material.py
@@ -0,0 +1,41 @@
+"""This module contains class describing isotropic material.
+   Class methods are used to generate lists with data required to define stress-strain curve.
+   Ramperg-Osgood relation for isotropic materials was used to define non-linearity of the material"""
+
+import math
+
+class mat:
+    def __init__(self, mat_name, Ftu, Fty, elongation, E):
+        self.mat_name= mat_name
+        self.Ftu = Ftu
+        self.Fty = Fty
+        self.elongation = elongation
+        self.E= E
+        self.n= math.log(self.Ftu/self.Fty)/math.log(self.elongation/0.2) 
+        # In original form natural logarithm was used. 
+        # Common logarithm do not 
+        self.H= self.Fty/(0.002**self.n)
+
+    def __str__(self):
+        _output = f'Material {self.mat_name} mechanical properties:\n   Young Modulus: {self.E}MPa\n   Ultimate strength: {self.Ftu}MPa\n   Yield strength: {self.Fty}MPa\n   Maximum elongation: {self.elongation}%'
+        return _output 
+
+    def generate_stress_list(self):
+        return [n for n in range(0, int(self.Ftu)+10, 1)]
+    
+    def generate_hooke_strain(self):
+        return [i/self.E for i in self.generate_stress_list()]
+
+    def generate_r_o_strain (self):
+        return [s/self.E + (s/self.H)**(1/self.n) for s in self.generate_stress_list()]
+
+
+if __name__ == "__main__":
+
+    test_material= mat("2024-T3",440.,340.,6.,70000.)
+    print(test_material)
+    material_name=str(test_material.mat_name)
+    print("\n\n   "+material_name)
+    
+
+
