a37cf3f3e4035ea51054df33dcbb1541eb9de1c3
Andrzej Machura <andrzej.jakub.machura@gmail.com>
2022-06-13 18:24:49 +0200

    script extended with menu and exceptions management

--- Stress-Strain_curve_generator.py
+++ Stress-Strain_curve_generator.py
@@ -5,9 +5,21 @@
    2. Chart with drawn Stress-Strain curve of given material.
 """
 import pylab
+from os import system
 import isotropic_material
 
-def generate_chart(Y):
+class MatNameError (Exception):
+    pass
+class NegativeValue (Exception):
+    pass
+class ModulusError (Exception):
+    pass
+class UltStrError (Exception):
+    pass
+class ElongationError (Exception):
+    pass
+
+def generate_chart(Y=isotropic_material.Mat("2024-T72", 470, 300, 11, 71000)):
 
     ultimate_elongation = [0., 1.1*max(Y.generate_r_o_strain())]
     ultimate_stress = [Y.Ftu, Y.Ftu]
@@ -23,16 +35,16 @@ def generate_chart(Y):
     pylab.savefig(str(Y.mat_name)+".jpg", dpi = 720) 
     pylab.show()
 
-def generate_text_file(X):
+def generate_text_file(X=isotropic_material.Mat("2024-T72", 470, 300, 11, 71000)):
     file = open(X.mat_name+".txt","w")
     file.write(str(X)+"\n")
     file.write("Row Number\tEngineering Strain\tEngineering Stress\tTrue Strain\tTrue Stress \n")
     i=0
-    while i < len(material.generate_stress_list()):
-        e_strain = "{:.3e}".format(material.generate_r_o_strain()[i])
-        e_stress = "{:.3e}".format(material.generate_stress_list()[i])
-        tr_strain = "{:.3e}".format(material.generate_true_strain()[i])
-        tr_stress = "{:.3e}".format(material.generate_true_stress()[i])
+    while i < len(X.generate_stress_list()):
+        e_strain = "{:.3e}".format(X.generate_r_o_strain()[i])
+        e_stress = "{:.3e}".format(X.generate_stress_list()[i])
+        tr_strain = "{:.3e}".format(X.generate_true_strain()[i])
+        tr_stress = "{:.3e}".format(X.generate_true_stress()[i])
         file.write(str(i+1)+"\t"+str(e_strain)+"\t"+str(e_stress)+"\t"+str(tr_strain)+"\t"+str(tr_stress)+"\n")
         i+=1
     
@@ -40,15 +52,88 @@ def generate_text_file(X):
 
 print("This script is responsible for generating non-linear Stress-Strain curve usig Ramberg-Osgood relation and linear curve acording to Hooke's law.")
 print("Required units are given in brackets \n")
-# material properties loading
 
-mat_name= input("Give material name: ")
-E = float (input("Give Younga modulus (MPa): "))
-Ftu = float (input("Give Ultimate Tensile Strength (MPa): "))
-Fty = float (input("Give Yield Tensile Strength (MPa): ")) 
-u = float (input("Give maximum elongation (%): "))
+while True:
+    print("Menu:\n")
+    print("1. Generate new curve")
+    print("2. Plot Stress-Strain chart (default:2024-T42)")
+    print("3. Generate output file (default 2024-T42)")
+    print("4. Exit\n") 
+    try: 
+        i= int(input("Print number to choose option:\n"))
+    except ValueError:
+        print("Menu option must be given")
+        system("PAUSE")
+        print("\n")
+        continue
 
-#create matrial object, generate stress-strain chart, create otput file
-material = isotropic_material.Mat(mat_name, Ftu, Fty, u, E)
-generate_chart(material)
-generate_text_file(material)
+    match i:
+        case 1:      
+            try:
+                mat_name= input("Give material name: ")
+                if mat_name =="": raise MatNameError
+                E = float (input("Give Younga modulus (MPa): "))
+                Ftu = float (input("Give Ultimate Tensile Strength (MPa): "))
+                Fty = float (input("Give Yield Tensile Strength (MPa): ")) 
+                u = float (input("Give maximum elongation (%): "))
+                material = isotropic_material.Mat(mat_name, Ftu, Fty, u, E)
+                
+                if E<0 or Ftu<0 or Fty<0 or u<0: raise NegativeValue
+                if E<Ftu: raise ModulusError
+                if Ftu<Fty: raise UltStrError
+                if u/100.<= 0.002: raise ElongationError
+
+            except MatNameError:
+                print("\n") 
+                print("Material should be named.\n")               
+                system("PAUSE")
+                print("\n")              
+                continue
+
+            except ValueError:
+                print("\n") 
+                print("All marterial physical properties should be given.\n")               
+                system("PAUSE")
+                print("\n")              
+                continue
+
+            except NegativeValue:
+                print("\n") 
+                print("All marterial physical properties should be positive numbers.\n")
+                system("PAUSE")
+                print("\n")                 
+                continue
+
+            except ModulusError:
+                print("\n")                 
+                print("Young Modulus should be greater than Ultimate Tensile Strength.\n")
+                system("PAUSE")
+                print("\n")                  
+                continue
+
+            except UltStrError:
+                print("\n")                
+                print("Ultimate Tensile Strength should be greater than Yield Tensile Strength.\n")
+                system("PAUSE")
+                print("\n")                
+                continue
+
+            except ElongationError:
+                print("\n")                
+                print('Maximum Elongation should be greater than 0.2%.\n')
+                system("PAUSE")
+                print("\n")                
+                continue                
+        case 2:
+            try:
+                generate_chart(material)
+            except NameError:
+                generate_chart()
+        
+        case 3:
+            try:
+                generate_text_file(material)
+            except NameError:
+                generate_text_file()
+        case 4:
+            break
